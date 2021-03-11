2996f774132427f29597a765ada8ad2e847fee65
Leonardo Furnielis <leonardofurnielis@outlook.com>
2021-03-11 12:30:45 -0300

    update test

--- test/services/read-recursive-directory.test.js
+++ test/services/read-recursive-directory.test.js
@@ -5,7 +5,7 @@ const readRecursiveDirectory = require('../../services/read-recursive-directory'
 describe('Read Recursive Directory', () => {
   test('When called, should return a list directory files', () => {
     const fileList = readRecursiveDirectory('/config');
-    expect(fileList).toHaveLength(10);
+    expect(fileList).toHaveLength(9);
   });
 
   test('When dicrectory not found, should return error ENOENT', () => {
