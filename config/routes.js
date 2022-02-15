ab624dd4046ac3ad82219f9a950e2aaca3fd00f8
Leonardo Furnielis <leonardofurnielis@outlook.com>
2022-02-15 16:08:43 -0300

    fix:eslint no-use-before-define

--- config/routes.js
+++ config/routes.js
@@ -8,6 +8,31 @@ const errorHandler = require('node-error-handler');
 // const swaggerUi = require('swagger-ui-express');
 // const swaggerDocument = require('swagger.json');
 
+/**
+ * Read recursive directory.
+ * @param {String} dir - The directory path to start read.
+ * @param {Array} filelist - List of directory.
+ * @returns {Array} - The complete list of directory.
+ */
+ const readRecursiveDirectory = (dir, filelist = []) => {
+  try {
+    const pathDir = path.join(process.cwd(), dir);
+    const files = fs.readdirSync(pathDir);
+    files.forEach((file) => {
+      if (fs.statSync(path.join(pathDir, file)).isDirectory()) {
+        filelist = readRecursiveDirectory(path.join(dir, file), filelist);
+      } else {
+        filelist.push(path.join(dir, file).replace(/(\\\\|\\)/g, '/'));
+      }
+    });
+
+    return filelist;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
+
 const routesLoader = (app) => {
   const fileList = readRecursiveDirectory('/src/api');
 
@@ -35,31 +60,6 @@ const routesLoader = (app) => {
   // Error handler
   const debug = process.env.LOGGER_LEVEL.toLowerCase() === 'debug' ? true : false;
   app.use(errorHandler({ debug }));
-};
-
-/**
- * Read recursive directory.
- * @param {String} dir - The directory path to start read.
- * @param {Array} filelist - List of directory.
- * @returns {Array} - The complete list of directory.
- */
-const readRecursiveDirectory = (dir, filelist = []) => {
-  try {
-    const pathDir = path.join(process.cwd(), dir);
-    const files = fs.readdirSync(pathDir);
-    files.forEach((file) => {
-      if (fs.statSync(path.join(pathDir, file)).isDirectory()) {
-        filelist = readRecursiveDirectory(path.join(dir, file), filelist);
-      } else {
-        filelist.push(path.join(dir, file).replace(/(\\\\|\\)/g, '/'));
-      }
-    });
-
-    return filelist;
-  } catch (err) {
-    console.error(err);
-    throw err;
-  }
 };
 
 module.exports = routesLoader;
